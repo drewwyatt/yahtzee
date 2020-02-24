024846893840ee2fc00e80dc8dc397cad9f82047
Drew Wyatt <drew.j.wyatt@gmail.com>
2020-02-23 23:43:30 -0500

    this may be working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use structopt::StructOpt;
 
 #[derive(StructOpt)]
@@ -6,6 +7,23 @@ struct Cli {
 }
 
 fn main() {
-  let args = Cli::from_args();
-  println!("rolls: {:?}", args.rolls);
+  let rolls = Cli::from_args().rolls;
+  let mut counts = HashMap::new();
+
+  for roll in rolls {
+    let count = counts.entry(roll).or_insert(0);
+    *count += 1;
+  }
+
+  let mut highest_score = 0;
+  for (roll, count) in counts {
+    let score = roll * count;
+    highest_score = if score > highest_score {
+      score
+    } else {
+      highest_score
+    };
+  }
+
+  println!("{}", highest_score)
 }
